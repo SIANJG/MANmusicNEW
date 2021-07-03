c4af1d060c4dc98e52ced7ea431da22f219f15de
SIANJG <86875523+SIANJG@users.noreply.github.com>
2021-07-03 21:37:40 +0700

    Update msg.py

--- DaisyXMusic/modules/msg.py
+++ DaisyXMusic/modules/msg.py
@@ -28,3 +28,30 @@ f"""
 """
 
       ]
+ 
+# Daisyxmusic (Telegram bot project )
+# Copyright (C) 2021  Inukaasith
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+from DaisyXMusic.config import ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP
+class Messages():
+      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 saya adalah bot yang memungkinkan untuk memutar musik di grup anda, Klik ADD ME TO YOUR GROUP untuk menambahkan saya ke grup anda, dan jangan lupa juga untuk menambahkan asisten saya, selengkapnya di /help ."
+      HELP_MSG = [
+        ".",
+f"""
+**👋 Halo [{}](tg://user?id={}) Baca daftar perintah terlebih dahulu!**
+"""
+      ]
