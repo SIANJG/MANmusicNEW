e9e358744cb5473beec35dcf26582c40c216d70f
SIANJG <86875523+SIANJG@users.noreply.github.com>
2021-07-04 09:38:11 +0700

    Update msg.py

--- DaisyXMusic/modules/msg.py
+++ DaisyXMusic/modules/msg.py
@@ -25,7 +25,7 @@ f"""
 [📝Klik disini](https://telegra.ph/Daftar-perintah-untuk-FeyyMusicBot-05-11)
 💬Atau hubungi Owner Project: [exKAMUUU](https://t.me/exKAMUUU)**
 
-"""
+        ".",
       ]
 
 # Editor by Creator And Contributor
