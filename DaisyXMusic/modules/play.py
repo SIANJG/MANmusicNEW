ab8663b243d410adf6386f21c76da153e9c5c0c9
SIANJG <86875523+SIANJG@users.noreply.github.com>
2021-07-09 14:02:39 +0700

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -130,7 +130,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((205, 630), f"Dilihat : {views}", (255, 255, 255), font=font)
     draw.text(
         (205, 670),
-        f"Diputar oleh : {requested_by}",format(message.chat.title)
+        f"Diputar oleh : {requested_by}",
         (255, 255, 255),
         font=font,
     )
